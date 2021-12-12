7f75bffd4e4b91c2a6fdb83822073b7bae4ee943
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-12 15:55:57 -0500

    color issue

--- RBshop.sh
+++ RBshop.sh
@@ -4,7 +4,7 @@ USER_UID="$(id -u)"
 
 if [ "${USER_UID}" -ne 0 ]; then
 
-  echo -e "/e[31m Please run the script as a root user /e[m"
+  echo -e"/e[31m Please run the script as a root user /e[m"
 
 exit
 fi
@@ -13,14 +13,14 @@ components=$1
 
 if [ -z "${components}" ]; then
 
-  echo -e "/e[33m Component Input is missing /e[m"
+  echo -e"/e[33m Component Input is missing /e[m"
 
 exit
 fi
 
 if [ -ne components/"${components}.sh" ]; then
 
-  echo -e "/e[34m Components Script is missing /e[m"
+  echo -e"/e[34m Components Script is missing /e[m"
 
 exit
 
