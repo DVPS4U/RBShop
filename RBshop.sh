9a4508e988cb800b70633000e2b3d44057a875c6
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-12 15:25:40 -0500

    Rbshop project and Components have been created

---
+++ RBshop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+user_id="${id -u}"
+
+if [ "${user_id}" -ne 0 ]; then
+
+  echo -e "/e[31m Please run the script as a root user /e[m"
+
+exit
+fi
+
+components=$1
+
+if [ -z "${components}" ]; then
+
+  echo -e "/e[33m Component Input is missing /e[m"
+
+exit
+fi
+
+if [ -ne components/${components}.sh ]; then
+
+  echo -e "/e[34m Components Script is missing /e[m"
+
+exit
+
+fi
+
+bash components/${components}.sh
