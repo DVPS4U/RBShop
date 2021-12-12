db04e0eeb3605a8977ca64ca95fbce9c80db7af0
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-12 16:40:11 -0500

    Installing NGINX and Downloading Frontend

--- Components/Frontend.sh
+++ Components/Frontend.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo Frontend Setup
+yum install nginx -y
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+systemctl enable nginx
+
+systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+cd /usr/share/nginx/html
+
+#rm -rf *
+
+#unzip /tmp/frontend.zip
+
+#mv frontend-main/* .
+
+#mv static/* .
+
+#rm -rf frontend-master static README.md
+
+#mv localhost.conf /etc/nginx/default.d/roboshop.conf
