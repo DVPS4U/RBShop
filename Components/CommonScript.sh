3ef4200a532ee636c01ee6da148b965662dba04e
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-13 21:27:48 -0500

    color code updated

--- Components/CommonScript.sh
+++ Components/CommonScript.sh
@@ -5,10 +5,10 @@ rm -f ${LOG_FILE}
 
 STAT_CHECK() {
   if [ $1 -ne 0 ];then
-  echo -e "\e[32m ${2} - FAILED /e[m"
+  echo -e "\e[32m ${2} - FAILED \e[m"
 
 exit 1
 else
-  echo -e "\e[32m ${2} - FAILED /e[m"
+  echo -e "\e[32m ${2} - FAILED \e[m"
 fi
 }
