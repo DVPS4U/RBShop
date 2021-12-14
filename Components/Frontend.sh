332d2e624f23f3cd59d0fcd8eadfdf0cd07703f1
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-13 21:17:46 -0500

    Front-end Script updated

--- Components/Frontend.sh
+++ Components/Frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install nginx -y &>> ${LOG_FILE}
+yum install nginx -y &>>${LOG_FILE}
 STAT_CHECK $? "Nginx Installation"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
@@ -12,7 +12,11 @@ STAT_CHECK $? "Remove Old HTML files"
 cd /tmp && unzip -o /tmp/frontend.zip &>> ${LOG_FILE}
 STAT_CHECK $? "Extracting Frontend Content"
 
-Cd /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+cd /tmp/frontend-main/static && cp -r * /usr/share/nginx/html
+STAT_CHECK $? "Copying Frontend Content"
+
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $? "Update NGINX File"
 
-system_ctl enable nginx &>> ${LOG_FILE} && system_ctl restart nginx &>> ${LOG_FILE}
+system_ctl enable nginx &>>${LOG_FILE} && system_ctl restart nginx &>>$ {LOG_FILE}
+STAT_CHECK $? "Restart Nginx"
