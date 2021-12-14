d61ffc2cc9cb385bae9f1313c350df2f740fee56
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-13 23:33:04 -0500

    MongoDB updated

--- Components/DATABASE.sh
+++ Components/DATABASE.sh
@@ -7,24 +7,18 @@ STAT_CHECK $? "MongoDB Download"
 yum install -y mongodb-org  &>> ${LOG_FILE}
 STAT_CHECK $? "MongoDB Installation"
 
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+sed -i 's/127.0.0.1 /0.0.0.0/' /etc/mongod.conf &>> ${LOG_FILE}
+STAT_CHECK $? "Update MongoD Service"
+
 systemctl enable mongod &>> ${LOG_FILE} && systemctl restart mongod &>> ${LOG_FILE}
 STAT_CHECK $? "MongoDB Restart"
 
-#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
 
-#Config file: /etc/mongod.conf
 
-#then restart the service
+Download mongodb
 
-# systemctl restart mongod
-Every Database needs the schema to be loaded for the application to work.
-Download the schema and load it.
-
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-
-# cd /tmp
-# unzip mongodb.zip
-# cd mongodb-main
-# mongo < catalogue.js
-# mongo < users.js
+cd mongodb-main
+mongo < catalogue.js &>> ${LOG_FILE} && mongo < users.js &>> ${LOG_FILE}
+STAT_CHECK $? "LOAD SCHEMA"
 
