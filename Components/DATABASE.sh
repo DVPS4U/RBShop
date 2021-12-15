a29e25c76d65de0893eb00868206ff170e0aaffa
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-14 22:20:04 -0500

    MongoDB updated

--- Components/DATABASE.sh
+++ Components/DATABASE.sh
@@ -23,3 +23,18 @@ mongo < catalogue.js &>>${LOG_FILE}
 mongo < users.js &>>${LOG_FILE}
 STAT_CHECK $? "LOAD SCHEMA"
 
+## Redis Setup
+
+#Install Redis.
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+STAT_CHECK $? "Redis Download"
+
+yum install redis -y &>>${LOG_FILE}
+STAT_CHECK $? "Redis Installation"
+
+#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+#sed -i -e 's/127.0.0.1/0.0.0.0'
+#Start Redis Database
+
+# systemctl enable redis
+# systemctl start redis
