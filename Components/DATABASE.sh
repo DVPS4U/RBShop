224f664464f43e65b1bfcc1f9826171faa2998ea
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-16 21:32:24 -0500

    MySQl error finding

--- Components/DATABASE.sh
+++ Components/DATABASE.sh
@@ -81,20 +81,20 @@ STAT_CHECK $? "MySQL Restarted"
 
 #Now a default root password will be generated and given in the log file.
 DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo 'SHOW DATABASE;' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
+#echo 'SHOW DATABASE;' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
 
-if [ $? -ne 0 ]; then
+#if [ $? -ne 0 ]; then
   echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" >/tmp/pass.sql
   mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/pass.sql  &>>${LOG_FILE}
   STAT_CHECK $? "Setup new Root Password"
-fi
+#fi
 
-echo 'show plugins;' | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
+#echo 'show plugins;' | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
 
-if [ $? -eq 0 ]; then
-echo 'uninstall plugin validate_password;' |  mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
-STAT_CHECK $? "Uninstall Password Plugin"
-fi
+#if [ $? -eq 0 ]; then
+#echo 'uninstall plugin validate_password;' |  mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
+#STAT_CHECK $? "Uninstall Password Plugin"
+#fi
 
 #curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
 #STAT_CHECK $? "mysql Downloaded"
