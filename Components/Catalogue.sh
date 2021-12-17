3e89c520e53c8c13d6e069f321996153a2201a3c
Shiju Raj <shijuraj.reny@gmail.com>
2021-12-16 23:11:42 -0500

    Catalogue conent copied to /home/roboshop/catalogue

--- Components/Catalogue.sh
+++ Components/Catalogue.sh
@@ -14,12 +14,13 @@ fi
 #So let's switch to the roboshop user and run the following commands.
 
 DOWNLOAD catalogue
-#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#cd /home/roboshop
-#unzip /tmp/catalogue.zip
-#mv catalogue-main catalogue
-#cd /home/roboshop/catalogue
-#npm install
+
+rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cp -r /tmp/catalogue-main/* /home/roboshop/catalogue &>>$"{LOG_FILE}"
+STAT_CHECK $? "Copy catalogue Content"
+
+
+cd /home/roboshop/catalogue && npm install &>>$"{LOG_FILE}"
+STAT_CHECK $? "Copy catalogue Content"
 #NOTE: We need to update the IP address of MONGODB Server in systemd.service file
 #Now, lets set up the service with systemctl.
 
